d200ba077c57fa64495ec2c60b189d92142ff8cf
Hiroki Kato <hirokikato1@gmail.com>
2023-07-30 21:40:39 -0400

    updates Links component

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from "react";
-
-function About() {
+import Links from "./Links"
+function About(props) {
+  
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {(props.bio && props.bio !== "") ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
 
-export default About;
+
+
+export default About
